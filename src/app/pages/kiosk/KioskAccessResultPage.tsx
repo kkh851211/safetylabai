090fb7d1dd03ffcedf0fc3906c22686d378dd13a
Jey <kkh851211@gmail.com>
2026-03-19 15:15:49 +0900

    K-3-01 출입 결과 표시 - 출입 허용

---
+++ src/app/pages/kiosk/KioskAccessResultPage.tsx
@@ -0,0 +1,12 @@
+import { KioskAccessResultScreen } from "../components/kiosk-access-result-screen";
+import { KioskPageLayout } from "../components/KioskPageLayout";
+
+export const title = "K-3-01 출입 결과 표시 - 허용";
+
+export default function KioskAccessResultPage() {
+  return (
+    <KioskPageLayout>
+      <KioskAccessResultScreen />
+    </KioskPageLayout>
+  );
+}
