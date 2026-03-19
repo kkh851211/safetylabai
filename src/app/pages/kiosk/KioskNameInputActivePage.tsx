2745a058c93715c35a62c0e9a843dc6a6d5f6025
Jey <kkh851211@gmail.com>
2026-03-19 13:35:24 +0900

    index 오류 개선

--- src/app/pages/kiosk/KioskNameInputActivePage.tsx
+++ src/app/pages/kiosk/KioskNameInputActivePage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { KioskPageLayout } from '../components/KioskPageLayout';
 import { KioskNameInputActiveScreen } from '../components/kiosk-name-input-active-screen';
+ 
+export const title = "K-2-02 수동 이름 입력 - 입력 중 (확인 버튼 활성화)";
 
 const KioskNameInputActivePage: React.FC = () => {
   return (
